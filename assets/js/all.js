fd9ef93430c2dc5223549ecdee415412b356cc20
圈圈 <62060936+vvvvvvii@users.noreply.github.com>
2020-12-14 18:02:01 +0800

    Update 2020-12-14T10:01:59.062Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,33 @@
 "use strict";
 
-console.log('Hello!');
-$(document).ready(function () {
-  console.log('HesSchool Hello!');
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  breakpoints: {
+    // when window width is >= 375px
+    375: {
+      slidesPerView: 'auto',
+      spaceBetween: 30
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 'auto',
+      spaceBetween: 30
+    },
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 4,
+      spaceBetween: 30
+    }
+  },
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
 });
 //# sourceMappingURL=all.js.map
